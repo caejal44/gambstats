ea4bb4eac68b05df5199bfbb4c64e54e1264296e
caejal44 <165009226+caejal44@users.noreply.github.com>
2026-06-15 05:59:06 -0400

    Create and style New Trip form

--- frontend/src/pages/NewTrip.tsx
+++ frontend/src/pages/NewTrip.tsx
@@ -1,11 +1,33 @@
 import Layout from "../components/Layout";
 import '../App.css';
 
+
+
 function NewTrip() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  console.log("Trip form submitted");
+}
   return (
     <Layout>
       <section>
         <h2 className="section-title">New Trip</h2>
+        <form id="newTrip" className="trip-form" onSubmit={handleSubmit}>
+          <label htmlFor="tripName">Trip Name</label>
+          <input type="text" id="tripName" name="tripName" placeholder="Enter trip name" required/>
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" name="location" placeholder="Enter location" required/>
+          <label htmlFor="tripBudget">Trip Budget</label>
+          <input type="number" id="tripBudget" name="tripBudget" placeholder="Enter trip budget" required/>
+          <label htmlFor="startedAt">Start Date</label>
+          <input type="date" id="startedAt" name="startedAt" placeholder="Enter start date" required/>
+          <label htmlFor="notes">Notes</label>
+          <textarea id="notes" name="notes" rows={4} placeholder="Enter notes (optional)"></textarea> 
+        <button type="submit">
+            Create Trip
+        </button>
+      </form>
       </section>
     </Layout>
   );
